6a0959993a096f972f9e3d8fa8ec9559db4bf67b
Saranjeet Kaur <kaur.saranjeet3@gmail.com>
2020-02-21 13:58:01 +0530

    Starting with nested sampling algo

---
+++ nested_sampling_algo.jl
@@ -0,0 +1,20 @@
+## Reference: https://en.wikipedia.org/wiki/Nested_sampling_algorithm
+## Computing marginal posterior probability density
+## More on defining prior_vector, iterations, likelihood_vector to be included
+likelihood = []
+X = []
+w = []
+Z = []
+function marginal_pd(N, prior_vector, iterations, likelihood_vector_current)
+    for i in 1:1:iterations
+        likelihood[i] = minimum(likelihood_vector_current) ## re-check this step
+        X[i] = exp(-i/N)
+        w[i] = X[i - 1] - X[i]
+        Z = Z + likelihood[i]*w[i]
+        ## save point with least likelihood as sample point with weight wi.
+        ## update point with least likelihood with some MCMC steps according to prior,
+        ## accepting only steps that keep the likelihood about Li
+        end
+    end
+    return Z
+end
